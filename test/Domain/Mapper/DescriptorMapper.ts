5d540eda046cbe4d7f533c7ac04a4a144c7b437f
Rogier Spieker <rogier@konfirm.io>
2023-10-17 08:54:27 +0200

    test for Symbol keys

--- test/Domain/Mapper/DescriptorMapper.ts
+++ test/Domain/Mapper/DescriptorMapper.ts
@@ -17,16 +17,17 @@ test('Domain/Mapper/DescriptorMapper - exports', (t) => {
 
 const { DescriptorMapper } = Export;
 
-test.only('Domain/Mapper/DescriptorMapper - get', (t) => {
+test('Domain/Mapper/DescriptorMapper - get', (t) => {
 	function clone(struct: object, ...invoke: Array<'freeze' | 'seal' | 'preventExtensions'>): typeof struct {
-		const cloned = Object.keys(struct).reduce((carry, key) => Object.assign(carry, { [key]: struct[key] }), {});
+		const cloned = { ...struct };
 
 		invoke.forEach((method) => (<any>Object[method as keyof Object])(cloned));
 
 		return cloned;
 	}
 
-	const standard = { foo: 1, bar: 2, baz: 3 };
+	const sym = Symbol();
+	const standard = { foo: 1, bar: 2, baz: 3, [sym]: 4 };
 	const standard_f = clone(standard, 'freeze');
 	const standard_s = clone(standard, 'seal');
 	const standard_p = clone(standard, 'preventExtensions');
@@ -36,32 +37,52 @@ test.only('Domain/Mapper/DescriptorMapper - get', (t) => {
 	const standard_fsp = clone(standard, 'freeze', 'seal', 'preventExtensions');
 
 	each`
-		target          | state                                 | key | value | writable | configurable
-		----------------|---------------------------------------|-----|-------|----------|--------------
-		${standard}     |                                       | foo | ${1}  | ${true}  | ${true}
-		${standard}     |                                       | new |       | ${true}  | ${true}
-		${standard_f}   | frozen                                | bar | ${2}  | ${false} | ${false}
-		${standard_f}   | frozen                                | new |       | ${false} | ${false}
-		${standard_s}   | sealed                                | baz | ${3}  | ${true}  | ${false}
-		${standard_s}   | sealed                                | new |       | ${true}  | ${false}
-		${standard_p}   | prevent extensions                    | foo | ${1}  | ${true}  | ${true}
-		${standard_p}   | prevent extensions                    | new |       | ${true}  | ${true}
-		${standard_fs}  | frozen and sealed                     | bar | ${2}  | ${false} | ${false}
-		${standard_fs}  | frozen and sealed                     | new |       | ${false} | ${false}
-		${standard_fp}  | frozen and prevent extensions         | baz | ${3}  | ${false} | ${false}
-		${standard_fp}  | frozen and prevent extensions         | new |       | ${false} | ${false}
-		${standard_sp}  | sealed and prevent extensions         | foo | ${1}  | ${true}  | ${false}
-		${standard_sp}  | sealed and prevent extensions         | new |       | ${true}  | ${false}
-		${standard_fsp} | frozen, sealed and prevent extensions | bar | ${2}  | ${false} | ${false}
-		${standard_fsp} | frozen, sealed and prevent extensions | new |       | ${false} | ${false}
+		target          | state                                 | key    | value | writable | configurable
+		----------------|---------------------------------------|--------|-------|----------|--------------
+		${standard}     |                                       | foo    | ${1}  | ${true}  | ${true}
+		${standard}     |                                       | bar    | ${2}  | ${true}  | ${true}
+		${standard}     |                                       | baz    | ${3}  | ${true}  | ${true}
+		${standard}     |                                       | ${sym} | ${4}  | ${true}  | ${true}
+		${standard}     |                                       | new    |       | ${true}  | ${true}
+		${standard_f}   | frozen                                | foo    | ${1}  | ${false} | ${false}
+		${standard_f}   | frozen                                | bar    | ${2}  | ${false} | ${false}
+		${standard_f}   | frozen                                | baz    | ${3}  | ${false} | ${false}
+		${standard_f}   | frozen                                | ${sym} | ${4}  | ${false} | ${false}
+		${standard_f}   | frozen                                | new    |       | ${false} | ${false}
+		${standard_s}   | sealed                                | foo    | ${1}  | ${true}  | ${false}
+		${standard_s}   | sealed                                | bar    | ${2}  | ${true}  | ${false}
+		${standard_s}   | sealed                                | baz    | ${3}  | ${true}  | ${false}
+		${standard_s}   | sealed                                | ${sym} | ${4}  | ${true}  | ${false}
+		${standard_s}   | sealed                                | new    |       | ${true}  | ${false}
+		${standard_p}   | prevent extensions                    | foo    | ${1}  | ${true}  | ${true}
+		${standard_p}   | prevent extensions                    | bar    | ${2}  | ${true}  | ${true}
+		${standard_p}   | prevent extensions                    | baz    | ${3}  | ${true}  | ${true}
+		${standard_p}   | prevent extensions                    | ${sym} | ${4}  | ${true}  | ${true}
+		${standard_p}   | prevent extensions                    | new    |       | ${true}  | ${true}
+		${standard_fs}  | frozen and sealed                     | foo    | ${1}  | ${false} | ${false}
+		${standard_fs}  | frozen and sealed                     | bar    | ${2}  | ${false} | ${false}
+		${standard_fs}  | frozen and sealed                     | baz    | ${3}  | ${false} | ${false}
+		${standard_fs}  | frozen and sealed                     | ${sym} | ${4}  | ${false} | ${false}
+		${standard_fs}  | frozen and sealed                     | new    |       | ${false} | ${false}
+		${standard_fp}  | frozen and prevent extensions         | foo    | ${1}  | ${false} | ${false}
+		${standard_fp}  | frozen and prevent extensions         | bar    | ${2}  | ${false} | ${false}
+		${standard_fp}  | frozen and prevent extensions         | baz    | ${3}  | ${false} | ${false}
+		${standard_fp}  | frozen and prevent extensions         | ${sym} | ${4}  | ${false} | ${false}
+		${standard_fp}  | frozen and prevent extensions         | new    |       | ${false} | ${false}
+		${standard_sp}  | sealed and prevent extensions         | foo    | ${1}  | ${true}  | ${false}
+		${standard_sp}  | sealed and prevent extensions         | bar    | ${2}  | ${true}  | ${false}
+		${standard_sp}  | sealed and prevent extensions         | baz    | ${3}  | ${true}  | ${false}
+		${standard_sp}  | sealed and prevent extensions         | ${sym} | ${4}  | ${true}  | ${false}
+		${standard_sp}  | sealed and prevent extensions         | new    |       | ${true}  | ${false}
+		${standard_fsp} | frozen, sealed and prevent extensions | foo    | ${1}  | ${false} | ${false}
+		${standard_fsp} | frozen, sealed and prevent extensions | bar    | ${2}  | ${false} | ${false}
+		${standard_fsp} | frozen, sealed and prevent extensions | baz    | ${3}  | ${false} | ${false}
+		${standard_fsp} | frozen, sealed and prevent extensions | ${sym} | ${4}  | ${false} | ${false}
+		${standard_fsp} | frozen, sealed and prevent extensions | new    |       | ${false} | ${false}
 	`(({ target, state, key, value, writable, configurable }: any) => {
 		const flags = { value, writable, enumerable: true, configurable };
 
-		console.dir({
-			native: Object.getOwnPropertyDescriptor(target, key),
-			descrp: DescriptorMapper.get(target, key),
-		}, { depth: null });
-		t.deepEqual(DescriptorMapper.get(target, key), flags, `${state} ${JSON.stringify(target)} has ${key} descriptor ${JSON.stringify(flags)}`);
+		t.deepEqual(DescriptorMapper.get(target, key), flags, `${state} ${JSON.stringify(target)} has ${key.toString()} descriptor ${JSON.stringify(flags)}`);
 	});
 
 	t.end();
