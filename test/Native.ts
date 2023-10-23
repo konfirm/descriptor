0b35bb4175a4112058344866e3a4b4757d1717cc
Rogier Spieker <rogier@konfirm.io>
2023-10-23 08:31:32 +0200

    Added tests for native behavior after Object.defineProperty

--- test/Native.ts
+++ test/Native.ts
@@ -2,6 +2,13 @@ import test from 'tape';
 import { each } from 'template-literal-each';
 import { DescriptorMapper } from '../source/Domain/Mapper/DescriptorMapper';
 
+function stringify(value: object): string {
+    const pairs = Object.keys(value)
+        .reduce((carry, key) => carry.concat(`${key}: ` + (typeof value[key] === 'function' ? 'Function' : JSON.stringify(value[key]))), [] as Array<string>);
+
+    return `{ ${pairs.join(', ')} }`;
+}
+
 class Test {
     public existing = 'value';
 }
@@ -57,6 +64,61 @@ test('Native - Object.getOwnPropertyDescriptor versus DescriptorMapper.get', (t)
             catch (_error) { }
         });
     })
+
+    t.end();
+});
+
+test('Native - Object.getOwnPropertyDescriptor versus DescriptorMapper.get after defineProperty', (t) => {
+    let value = 0;
+
+    each`
+		writable | enumerable | configurable 
+		---------|------------|--------------
+		         |            | 
+		${true}  |            | 
+		${false} |            | 
+		         | ${true}    | 
+		         | ${false}   | 
+		         |            | ${true}
+		         |            | ${false}
+        ${true}  | ${true}    |
+        ${false} | ${true}    |
+        ${true}  | ${false}   |
+        ${false} | ${false}   |
+        ${true}  |            | ${true}
+        ${false} |            | ${true}
+        ${true}  |            | ${false}
+        ${false} |            | ${false}
+                 | ${true}    | ${true}
+                 | ${false}   | ${true}
+                 | ${true}    | ${false}
+                 | ${false}   | ${false}
+        ${true}  | ${true}    | ${true}
+        ${false} | ${true}    | ${true}
+        ${true}  | ${false}   | ${true}
+        ${true}  | ${true}    | ${false}
+        ${false} | ${false}   | ${true}
+        ${false} | ${true}    | ${false}
+        ${true}  | ${false}   | ${false}
+        ${false} | ${false}   | ${false}
+	`(({ writable, enumerable, configurable }: any) => {
+        [
+            { value: ++value, writable },
+            { get: () => value, set: undefined },
+            { get: () => value, set: () => { } },
+            { get: undefined, set: () => { } },
+        ].forEach((append) => {
+            const target = {};
+            const define = { ...append, enumerable, configurable };
+            const expect = { ...append, ...('writable' in append ? { writable: writable || false } : {}), enumerable: enumerable || false, configurable: configurable || false };
+
+            Object.defineProperty(target, 'new', define);
+
+            const descriptor = DescriptorMapper.get(target, 'new');
+            t.deepEqual(descriptor, expect, `defined ${stringify(define)} returns ${stringify(expect)}`);
+            t.deepEqual(descriptor, Object.getOwnPropertyDescriptor(target, 'new'), `Object.getOwnPropertyDescriptor returns ${stringify(expect)}`);
+        });
+    });
 
     t.end();
 });
